2f3a9488b39ea6f89625dca8ea7fe2dcdfbc3760
Cody Yantis <cyantis@gmail.com>
2019-03-15 23:14:49 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,7 +16,6 @@ class CashRegister
     for i in 1..quantity
       @items << item
     end
-    price
   end
 
   def apply_discount
@@ -33,7 +32,7 @@ class CashRegister
   end
 
   def void_last_transaction
-    self.total - self.add_item(item, price, quantity = 1)
+
   end
 
 end
